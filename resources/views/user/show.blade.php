1b062b0680ce1decf77bb4f6dd2606c3b8432dca
keiki-saito <keicake0808@gmail.com>
2021-02-11 16:32:53 +0900

    deploy

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -9,7 +9,7 @@
                 <div class="row no-gutters">
                     <div class="col-md-4">
                         <!-- <img class="round-img ml-4 mt-4" src="{{ asset('/images/' . $user->profile_img) }}" /> -->
-                        <img src="$user->profile_img" alt="ユーザー画像" class="round-img ml-4 mt-4>
+                        <img src="{{$user->profile_img}}" alt="ユーザー画像" class="round-img ml-4 mt-4>
                     </div>
                     <div class="col-md-8">
                         <div class="card-title">
