cbb7ec02786a2fe6a2aff6d587b5d1fa764278c4
keiki-saito <keicake0808@gmail.com>
2021-02-11 16:03:19 +0900

    modify controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,47 +73,56 @@ class RegisterController extends Controller
         //\Debugbar::addMessage($value);
         //dd($data);
 
-            $imageFile = $data['profile_img'];
+            // $imageFile = $data['profile_img'];
 
-            //$imageFileからファイル名を取得(拡張子あり)
-            $filenameWithExt = $imageFile->getClientOriginalName();
+            // //$imageFileからファイル名を取得(拡張子あり)
+            // $filenameWithExt = $imageFile->getClientOriginalName();
 
-            //拡張子を除いたファイル名を取得
-            $fileName = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            // //拡張子を除いたファイル名を取得
+            // $fileName = pathinfo($filenameWithExt, PATHINFO_FILENAME);
 
-            //拡張子を取得
-            $extension = $imageFile->getClientOriginalExtension();
+            // //拡張子を取得
+            // $extension = $imageFile->getClientOriginalExtension();
 
-            // ファイル名_時間_拡張子 として設定
-            $fileNameToStore = $fileName.'_'.time().'.'.$extension;
+            // // ファイル名_時間_拡張子 として設定
+            // $fileNameToStore = $fileName.'_'.time().'.'.$extension;
 
-            //画像ファイル取得
-            $fileData = file_get_contents($imageFile->getRealPath());
+            // //画像ファイル取得
+            // $fileData = file_get_contents($imageFile->getRealPath());
 
-            //拡張子ごとに base64エンコード実施
-            if ($extension === 'jpg') {
-                $data_url = 'data:image/jpg;base64,'. base64_encode($fileData);
-            }
+            // //拡張子ごとに base64エンコード実施
+            // if ($extension === 'jpg') {
+            //     $data_url = 'data:image/jpg;base64,'. base64_encode($fileData);
+            // }
 
-            if ($extension === 'jpeg') {
-                $data_url = 'data:image/jpg;base64,'. base64_encode($fileData);
-            }
+            // if ($extension === 'jpeg') {
+            //     $data_url = 'data:image/jpg;base64,'. base64_encode($fileData);
+            // }
 
-            if ($extension === 'png') {
-                $data_url = 'data:image/png;base64,'. base64_encode($fileData);
-            }
+            // if ($extension === 'png') {
+            //     $data_url = 'data:image/png;base64,'. base64_encode($fileData);
+            // }
 
-            if ($extension === 'gif') {
-                $data_url = 'data:image/gif;base64,'. base64_encode($fileData);
-            }
+            // if ($extension === 'gif') {
+            //     $data_url = 'data:image/gif;base64,'. base64_encode($fileData);
+            // }
 
-            //画像アップロード(Imageクラス makeメソッドを使用)
-            $image = Image::make($data_url);
+            // //画像アップロード(Imageクラス makeメソッドを使用)
+            // $image = Image::make($data_url);
 
-            //画像を横400px, 縦400pxにリサイズし保存
-            // $image->resize(400, 400)->save(storage_path() . '/app/public/images/' . $fileNameToStore);
-            // ---ここまで追加---
-            $path =Storage::disk('s3')->putFile('/', $fileNameToStore, 'public');
+            // //画像を横400px, 縦400pxにリサイズし保存
+            // // $image->resize(400, 400)->save(storage_path() . '/app/public/images/' . $fileNameToStore);
+            // // ---ここまで追加---
+            // $path =Storage::disk('s3')->putFile('/', $fileNameToStore, 'public');
+
+
+      //s3アップロード開始
+            $image = $data('image');
+      // バケットの`myprefix`フォルダへアップロード
+         $path = Storage::disk('s3')->putFile('/', $image, 'public');
+    
+
+
 
 
 
